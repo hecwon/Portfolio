7578035fb890897164090d91dc575f63ac33e0dc
hecwon <hecwon@gmail.com>
2023-06-10 19:40:20 -0700

    wrap blobPosUpdate in useEffect to provide access to window

--- components/blob/blob.tsx
+++ components/blob/blob.tsx
@@ -1,25 +1,27 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
 import styles from './blob.module.css';
 
 export default function Blob () {
-  let blob: HTMLElement | null
 
-  if (window) {
-    window.addEventListener('mousemove', (event) => {
-      updateBlobPos(event.clientX, event.clientY);
-    })
-  }
-
-  const updateBlobPos: (x: number, y: number) => void = (x, y) => {
-    if (!blob) {
-      blob = document.getElementById('blob');
+  useEffect(() => {
+    const updateBlobPos: (x: number, y: number) => void = (x, y) => {
+      let blob: HTMLElement | null = document.getElementById('blob');
+      blob!.animate({
+        left: `${x}px`,
+        top: `${y}px`,
+      }, {duration: 3000, fill: 'forwards'})
     }
-    blob!.animate({
-      left: `${x}px`,
-      top: `${y}px`,
-    }, {duration: 3000, fill: 'forwards'})
-  }
+
+    if (typeof window !== "undefined") {
+      window.addEventListener('mousemove', (event) => {
+        updateBlobPos(event.clientX, event.clientY);
+      })
+    }
+
+  }, [])
+
   return (
     <>
       <div className={styles.blur}>
