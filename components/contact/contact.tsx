9d0b13542f93e569a48360c42141ec1add61c8b3
hecwon <hecwon@gmail.com>
2023-06-10 21:50:42 -0700

    update contact form accesskey parameters and test work.description adjustment

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,7 +1,7 @@
 import styles from './contact.module.css';
+import 'dotenv/config';
 
 export default function Contact () {
-  const accessKey="12345";
   return (
     <div className={styles.contactContainer}>
       <h1 className={styles.title}>
@@ -9,7 +9,7 @@ export default function Contact () {
       </h1>
         <form action='https://api.web3forms.com/submit' method='POST'>
           <div className={styles.formGrid}>
-            <input type='hidden' name='access_key' value={accessKey}/>
+            <input type='hidden' name='access_key' value={process.env.accessKey}/>
             <input className={styles.name} placeholder='Name' required/>
             <input className={styles.email} placeholder='Email' required/>
             <textarea className={styles.message} placeholder='Message' required/>
